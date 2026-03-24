53f3bbf954e9a355bbde68857363a1a0084cc289
Martin Algreen <git@mal.green>
2026-03-24 11:41:31 +0100

    SimulationService: add docstring and improve notebook

--- services/simulation_service.py
+++ services/simulation_service.py
@@ -9,6 +9,25 @@ from roboticstoolbox.tools.trajectory import Trajectory
 from spatialmath import SE3
 
 class SimulationService:
+    """
+    The SimulationService class provides simulation functionality to the digital win.
+    It takes control messages resembling the messages for the mockup:
+    ```py
+    ctrl: dict = {
+        protocol.CtrlMsgKeys.TYPE: protocol.CtrlMsgFields.LOAD_PROGRAM,
+        protocol.CtrlMsgKeys.JOINT_POSITIONS: [0, np.pi/2, 0, 0, 0, 0]
+    }
+    ```
+    NOTE: it does not require a `START` message. It simulates when receiving `LOAD_PROGRAM`.\\
+    After a simulation pass, it publishes the state. The message structure for the state,
+    resembles the structure of the mockup state messages:
+    ```py
+    state: dict = {
+        protocol.RobotArmStateKeys.Q_ACTUAL: [0, 0, 0, 0, 0, 0],
+        protocol.RobotArmStateKeys.TCP_POSE: [x, y, z, r, p, y]            
+    }
+    ```
+    """
     def __init__(self) -> None:
         # === RabbitMQ === #
         self.rmq: Rabbitmq = Rabbitmq(
@@ -39,59 +58,12 @@ class SimulationService:
 
     def on_ctrl_message_received(self, channel, method, properties, body) -> None:
         try:
-            print(type(body))
             if body.get(protocol.CtrlMsgKeys.TYPE) == protocol.CtrlMsgFields.LOAD_PROGRAM:
                 target_joint_positions = body.get(protocol.CtrlMsgKeys.JOINT_POSITIONS)
                 self.head = self.model.fkine(target_joint_positions)
                 self.rmq.send_message(protocol.ROUTING_KEY_SIM_STATE, {
                     protocol.RobotArmStateKeys.Q_ACTUAL: self.model.q.tolist(),
                     protocol.RobotArmStateKeys.TCP_POSE: [self.head.x, self.head.y, self.head.z] + self.head.rpy().tolist()
-                    
                 })
         except Exception as e:
             print(e)
-
-
-    # def on_state_message_received(self, channel, method, properties, body) -> None:
-    #     try:
-            
-    #     except Exception as e:
-    #         print(e)
-
-
-
-class UR3eControllerModel:
-    pass
-
-
-class UR3eRoboticArmModel:
-    def __init__(self) -> None:
-        self.links: list[rtb.RevoluteDH] = [
-            rtb.RevoluteDH(d=0.15185, a=0, alpha=np.pi/2),
-            rtb.RevoluteDH(d=0, a=-0.24355, alpha=0),
-            rtb.RevoluteDH(d=0, a=-0.2132, alpha=0),
-            rtb.RevoluteDH(d=0.13105, a=0, alpha=np.pi/2),
-            rtb.RevoluteDH(d=0.08535, a=0, alpha=-np.pi/2),
-            rtb.RevoluteDH(d=0.0921, a=0, alpha=0),
-        ]
-        self.model: rtb.DHRobot = rtb.DHRobot(name="UR3e Model", links=self.links)
-        self.joints: list[int | float] = [0, 0, 0, 0, 0, 0] # TODO
-        self.trajectory: Trajectory
-        self.counter: int = 0
-    
-
-    def do_fk(self, rotations: list[int | float]) -> SE3:
-        assert len(rotations) == len(self.model.links), "Length of joint rotations must match length of model links"
-        self.joints = rotations
-        return self.model.fkine(rotations)
-    
-
-    # def do_ik(self) -> None:
-    def start_trajectory(self, start: list[int | float], end: list[int | float], steps: int) -> None:
-        self.trajectory = rtb.jtraj(start, end, steps)
-
-
-    def do_step(self) -> _UnknownType:
-        current = self.trajectory.q[self.counter]
-        self.counter += 1
-        return current
