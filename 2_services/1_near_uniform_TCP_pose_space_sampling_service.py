0385b2231f9b47fb48b5a7cc1b14270f3812cb44
Nikan8203 <125121042+Nikan8203@users.noreply.github.com>
2026-03-24 09:34:46 +0100

    Added the Sampling and ML services

---
+++ 2_services/1_near_uniform_TCP_pose_space_sampling_service.py
@@ -0,0 +1,3 @@
+# Here we will create a near-uniform TCP pose space sampling service, which we will primary use for 
+# sampling the TCP pose space of the mockup and our model, which is in turn used for a machine 
+# learning-based error correction/calibration service. 
---
+++ 2_services/2_calibration_machine_learning_service.py
@@ -0,0 +1,5 @@
+
+# Here we will create an ML service for estimating the error between the mockup (PT) and the 
+# emulator (our model) using the near-uniform TCP pose space sampling from before, to achieve
+# TCP pose calibration. 
+
