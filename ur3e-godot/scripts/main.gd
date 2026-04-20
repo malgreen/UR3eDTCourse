2710b26fb0466ecf29191a0dd0fee512adb48479
Martin Algreen <git@mal.green>
2026-04-20 10:11:39 +0200

    3dviz: add joint rotation offsets

--- ur3e-godot/scripts/main.gd
+++ ur3e-godot/scripts/main.gd
@@ -1,11 +1,36 @@
 extends Node3D
 
+@onready var ur3e: UR3e = $UR3e
+@onready var rabbit_mq_listener: Node = $RabbitMQListener
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	rabbit_mq_listener.connect("OnMessage", _on_message_received)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_message_received(msg: String):
+	var dict: Dictionary = JSON.parse_string(msg)
+	print("MESSAGE RCV")
+	#print(msg)
+	if dict.get("q_actual") != null:
+		ur3e.rotate_joints(dict["q_actual"])
+		
+
+## this is how the message looks:
+# q_actual = rotations
+# qd_actual = velocities
+#{
+#	"robot_mode": "Idle", 
+#	"q_actual": [-5.091937626080412e-05, -1.5700026650473495, 1.5700010478123378, -1.570024065384959, -1.570001209062341, 3.7861117572591774e-05], 
+#	"qd_actual": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], 
+#	"q_target": [0.0, -1.57, 1.57, -1.57, -1.57, 0.0], 
+#	"timestamp": 2182.499999999174, 
+#	"joint_max_speed": 60, 
+#	"joint_max_acceleration": 80, 
+#	"tcp_pose": [-0.2986799840661358, -0.13111885356249206, 0.3032406590875115, -1.5707069335491348, 0.0007706034434146294, 3.1407974963814573]
+#}
--- ur3e-godot/scripts/ur3e.gd
+++ ur3e-godot/scripts/ur3e.gd
@@ -1,19 +1,27 @@
+class_name UR3e
 extends Node3D
 
+# we need the *_holder nodes to essentially have some rotation offset
 @onready var joints: Array[Marker3D] = [
 	$v2/J0/J1, 
-	$v2/J0/J1/J2, 
-	$v2/J0/J1/J2/J3, 
-	$v2/J0/J1/J2/J3/J4,
-	$v2/J0/J1/J2/J3/J4/J5,
-	$v2/J0/J1/J2/J3/J4/J5/J6,
+	$v2/J0/J1/J2_holder/J2, 
+	$v2/J0/J1/J2_holder/J2/J3, 
+	$v2/J0/J1/J2_holder/J2/J3/J4_holder/J4,
+	$v2/J0/J1/J2_holder/J2/J3/J4_holder/J4/J5,
+	$v2/J0/J1/J2_holder/J2/J3/J4_holder/J4/J5/J6,
 ]
 
 func rotate_joints(rotations: Array) -> void:
 	assert(len(rotations) == len(joints), "Input count must match joint count")
 	print(rotations)
-	#for i in rotations.size:
-		#joints[i].global_rotate(rotations[i])
+	for i in rotations.size():
+		#joints[i].global_rotation.y = rotations[i]
+		match i:
+			0, 4:
+				joints[i].rotation.y = rotations[i]
+			1, 2, 3, 5:
+				joints[i].rotation.z = rotations[i]
+			
 		
 	
 
